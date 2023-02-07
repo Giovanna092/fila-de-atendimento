9b5beb46c705c2617cb8f25362fb9bb47b685c0d
Giovanna Costa <giovanna.developerbe@gmail.com>
2023-02-07 16:58:49 -0300

    simulacao de uma fila de atendimento

---
+++ src/Fila.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fila {
+    public static void main(String[] args) {
+        
+        Scanner leia = new Scanner(System.in);
+
+        int p;
+        System.out.println("Digite o numero de pessoas que deseja adicionar na fila");
+        p = leia.nextInt();
+        String[] nomes = new String[p];
+        
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println("Digite o nome");
+            nomes[i] = leia.next();
+        }
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]+ " - esta na posicao: "+ (i+1));
+        }
+    }
+}
